210fa687e4b149bb5599368f9c22144e3177dbc4
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-12-08 09:49:32 -0700

    Added searcbar from react-native-paper library

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import { useState } from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
 
+import { Searchbar } from "react-native-paper";
+
 export default function App() {
+	// searchbar state
+	const [searchQuery, setSearchQuery] = useState("");
+
+	// searchbar onchange handler
+	const onChangeSearch = (query) => setSearchQuery(query);
 	return (
 		<>
 			<SafeAreaView style={styles.container}>
 				<View style={styles.search}>
-					<Text>search</Text>
+					<Searchbar
+						placeholder="Search"
+						onChangeText={onChangeSearch}
+						value={searchQuery}
+					/>
 				</View>
 				<View style={styles.list}>
 					<Text>list</Text>
