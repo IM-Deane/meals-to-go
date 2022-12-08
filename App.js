af56755099784584794d19419acfe35cc08f6242
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-12-07 18:05:03 -0700

    Added initial view template Added linter and fixed errors

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.search}>
+					<Text>search</Text>
+				</View>
+				<View style={styles.list}>
+					<Text>list</Text>
+				</View>
+			</SafeAreaView>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: { flex: 1, marginTop: StatusBar.currentHeight },
+	search: { backgroundColor: "green", padding: 16 },
+	list: { flex: 1, backgroundColor: "blue", padding: 16 },
 });
